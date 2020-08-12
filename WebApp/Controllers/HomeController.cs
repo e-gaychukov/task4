87a0eb6ecafd4c7a06493588c7a60a6f6d153c27
Egor Gaychukov <egor.gaychukov@list.ru>
2020-08-12 21:43:50 +0300

    h

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.Owin;
+using WebApp.Filtres;
+using System.Reflection;
+
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    [CheckUser]
+    public class HomeController : Controller
+    {
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+        }
+        public ActionResult Index()
+        {
+            return View(UserManager.Users.ToArray());
+        }
+        public ActionResult BlockUsers(string[] selected)
+        {
+            Array.ForEach(selected, id => UserManager.SetLockoutEnabledAsync(id, true).Wait());
+            return View("Index", UserManager.Users.ToArray());
+        }
+        public ActionResult UnblockUsers(string[] selected)
+        {
+            Array.ForEach(selected, id => UserManager.SetLockoutEnabledAsync(id, false).Wait());
+            return View("Index", UserManager.Users.ToArray());
+        }
+        public ActionResult DeleteUsers(string[] selected)
+        {
+            Array.ForEach(selected, id => UserManager.DeleteAsync(UserManager.FindByIdAsync(id).Result).Wait());
+            return View("Index", UserManager.Users.ToArray());
+        }
+        public ApplicationUser GetCurrentUser()
+        {
+            var userManager = HttpContext.Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            return userManager.FindByNameAsync(HttpContext.User.Identity.Name).Result;
+        }
+    }
+}
---
+++ WebApp/Filtres/CheckUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.Owin;
+
+
+namespace WebApp.Filtres
+{
+    public class CheckUser : FilterAttribute, IActionFilter
+    {
+        public CheckUser() { }
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            RedirectBannedUsers(filterContext.HttpContext, () => { filterContext.Result = null; });
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            RedirectBannedUsers(filterContext.HttpContext, () => { filterContext.Result = null; });
+        }
+
+        public void RedirectBannedUsers(HttpContextBase context, Action extraActions)
+        {
+            if (GetCurrentUser(context) == null || GetCurrentUser(context).LockoutEnabled)
+            {
+                extraActions();
+                context.GetOwinContext().Authentication.SignOut();
+                context.Response.RedirectToRoute("Default", new { action = "Index", controller = "Home" });
+            }
+        }
+
+        public ApplicationUser GetCurrentUser(HttpContextBase context)
+        {
+            var userManager = context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            return userManager.FindByNameAsync(context.User.Identity.Name).Result;
+        }
+    }
+}
